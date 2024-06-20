cd9aa65d1854139415cd299e40d8d5bd652d3954
Toutouchien <toutouchienshiba@gmail.com>
2024-06-20 14:34:46 +0200

    Make an enum to a display name

--- src/main/java/toutouchien/niveriaapi/utils/StringUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/StringUtils.java
@@ -7,4 +7,16 @@ public class StringUtils {
 
 		return string.toUpperCase().charAt(0) + string.toLowerCase().substring(1);
 	}
+
+	public static String enumToDisplayName(String toConvert) {
+		String[] parts = toConvert.split("_");
+		if(parts.length == 0)
+			return "";
+
+		StringBuilder displayName = new StringBuilder();
+		for (int i = 0; i < parts.length; i++)
+			displayName.append(capitalize(parts[i]));
+
+		return displayName.toString();
+	}
 }
