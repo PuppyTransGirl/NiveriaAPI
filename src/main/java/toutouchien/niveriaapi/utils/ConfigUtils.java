3a88fd2c41988b61645ca02f17cafdd4455337bd
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2024-07-30 22:49:28 +0200

    Add ConfigUtils and Update the version to 1.3.0

---
+++ src/main/java/toutouchien/niveriaapi/utils/ConfigUtils.java
@@ -0,0 +1,35 @@
+package toutouchien.niveriaapi.utils;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.ConfigurationSection;
+
+import java.text.DecimalFormat;
+
+public class ConfigUtils {
+	// It's to make a 1/16 (0.625) precision like the pixel of a block
+	private final static DecimalFormat decimalFormat = new DecimalFormat("#.####");
+
+	public static void setLocation(ConfigurationSection section, Location location) {
+		String world = location.getWorld().getName();
+		double x = roundDouble(location.x());
+		double y = roundDouble(location.y());
+		double z = roundDouble(location.z());
+		float yaw = roundFloat(location.getYaw());
+		float pitch = roundFloat(location.getPitch());
+
+		section.set("world", world);
+		section.set("x", x);
+		section.set("y", y);
+		section.set("z", z);
+		section.set("yaw", yaw);
+		section.set("pitch", pitch);
+	}
+
+	private static double roundDouble(double d) {
+		return Double.parseDouble(decimalFormat.format(d));
+	}
+
+	private static float roundFloat(float f) {
+		return Float.parseFloat(decimalFormat.format(f));
+	}
+}
