40d86beaa46843dff0277cf494592c6430c367b8
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2024-08-16 18:54:19 +0200

    Fix DecimalFormat returning numbers with a commas

--- src/main/java/toutouchien/niveriaapi/utils/ConfigUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/ConfigUtils.java
@@ -4,10 +4,15 @@ import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
 
 public class ConfigUtils {
 	// It's to make a 1/16 (0.625) precision like the pixel of a block
-	private final static DecimalFormat decimalFormat = new DecimalFormat("#.####");
+	private final static DecimalFormat decimalFormat = new DecimalFormat("#.####", new DecimalFormatSymbols(Locale.US));
+	static {
+		decimalFormat.setGroupingUsed(false);
+	}
 
 	public static void setLocation(ConfigurationSection section, Location location) {
 		String world = location.getWorld().getName();
