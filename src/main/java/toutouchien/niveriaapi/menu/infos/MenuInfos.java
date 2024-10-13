7394d6edb6a19fe68e9b5720b99d84b28ebe3ed2
Toutouchien <toutouchienshiba@gmail.com>
2024-10-13 12:58:19 +0200

    Add material to MenuInfos

--- src/main/java/toutouchien/niveriaapi/menu/infos/MenuInfos.java
+++ src/main/java/toutouchien/niveriaapi/menu/infos/MenuInfos.java
@@ -1,14 +1,21 @@
 package toutouchien.niveriaapi.menu.infos;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
 
 public class MenuInfos {
 	private final Player player;
+	private Material material;
 
 	public MenuInfos(Player player) {
 		this.player = player;
+	}
+
+	public MenuInfos(Player player, Material material) {
+		this.player = player;
+		this.material = material;
 	}
 
 	public Player player() {
@@ -17,5 +24,13 @@ public class MenuInfos {
 
 	public UUID uuid() {
 		return player.getUniqueId();
+	}
+
+	public void material(Material material) {
+		this.material = material;
+	}
+
+	public Material material() {
+		return material;
 	}
 }
