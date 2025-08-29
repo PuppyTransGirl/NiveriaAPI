012a244c1e93cc2dae0bb129479c66dae72730ee
Toutouchien <puppytransgirl+github@proton.me>
2025-08-29 21:05:14 +0200

    Use a mirror instead of maven central

--- src/main/java/toutouchien/niveriaapi/NiveriaAPILoader.java
+++ src/main/java/toutouchien/niveriaapi/NiveriaAPILoader.java
@@ -12,7 +12,7 @@ public class NiveriaAPILoader implements PluginLoader {
     public void classloader(PluginClasspathBuilder classpathBuilder) {
         MavenLibraryResolver resolver = new MavenLibraryResolver();
         resolver.addDependency(new Dependency(new DefaultArtifact("org.mongodb:mongodb-driver-sync:5.4.0"), null));
-        resolver.addRepository(new RemoteRepository.Builder("central", "default", "https://repo1.maven.org/maven2/").build());
+        resolver.addRepository(new RemoteRepository.Builder("central", "default", MavenLibraryResolver.MAVEN_CENTRAL_DEFAULT_MIRROR).build());
 
         classpathBuilder.addLibrary(resolver);
     }
