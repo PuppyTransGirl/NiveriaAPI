a81b8b88b943b6a104dec1d0c7978ec3a3e211f0
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-09 23:16:45 +0100

    Transition from a Maven project to a Gradle project (#20)
    * Transition (:3) from a Maven project to a Gradle project
    
    * Fix github build
    
    * Change name
    
    * Try again
    
    * Try shorter name
    
    * Try other things
    
    * Even simpler
    
    * Aaaaa
    
    * Shoka - Ado
    
    * test
    
    * test V2
    
    * maybe this
    
    * Add workflows
    
    * Not really useful
    
    * Add config file for releases
    
    * Update dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,93 @@
+plugins {
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("xyz.jpenilla.run-paper") version "2.3.1"
+}
+
+val minecraftVersion: String by project
+val placeholderApiVersion: String by project
+val luckpermsVersion: String by project
+val itemsAdderVersion: String by project
+val worldguardVersion: String by project
+val landsVersion: String by project
+val bluemapVersion: String by project
+val pl3xmapVersion: String by project
+val squaremapVersion: String by project
+val dynmapVersion: String by project
+val mongoDBVersion: String by project
+val mockbukkitVersion: String by project
+
+group = "toutouchien.niveriaapi"
+version = "2.1.0"
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.bluecolored.de/releases")
+    maven("https://repo.mikeprimm.com/")
+    maven("https://maven.devs.beer/")
+    maven("https://jitpack.io")
+    maven("https://api.modrinth.com/maven/")
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("${minecraftVersion}-R0.1-SNAPSHOT")
+
+    // Plugins
+    compileOnly("me.clip:placeholderapi:${placeholderApiVersion}")
+    compileOnly("net.luckperms:api:${luckpermsVersion}")
+    compileOnly("dev.lone:api-itemsadder:${itemsAdderVersion}")
+    compileOnly("com.sk89q.worldguard:worldguard-bukkit:${worldguardVersion}")
+    compileOnly("com.github.angeschossen:LandsAPI:${landsVersion}")
+    compileOnly("de.bluecolored:bluemap-api:${bluemapVersion}")
+    compileOnly("maven.modrinth:pl3xmap:${pl3xmapVersion}")
+    compileOnly("xyz.jpenilla:squaremap-api:${squaremapVersion}")
+    compileOnly("us.dynmap:dynmap-api:${dynmapVersion}")
+    compileOnly("us.dynmap:DynmapCoreAPI:${dynmapVersion}")
+
+    // Dependencies
+    compileOnly("org.mongodb:mongodb-driver-sync:${mongoDBVersion}")
+
+    // Test Dependencies
+    testImplementation(paperweight.paperDevBundle("${minecraftVersion}-R0.1-SNAPSHOT"))
+    testImplementation("org.mockbukkit.mockbukkit:mockbukkit-v1.21:${mockbukkitVersion}")
+}
+
+paperweight {
+    addServerDependencyTo = configurations.named(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).map { setOf(it) }
+}
+
+tasks {
+    runServer {
+        minecraftVersion(minecraftVersion)
+
+        jvmArgs(
+            "-Xmx4096M",
+            "-Xms4096M",
+            "-XX:+AllowEnhancedClassRedefinition",
+            "-XX:HotswapAgent=core",
+            "-Dcom.mojang.eula.agree=true"
+        )
+    }
+
+    build {
+        dependsOn("jar")
+    }
+
+    processResources {
+        filteringCharset = "UTF-8"
+
+        val props = mapOf(
+            "version" to version,
+            "minecraftVersion" to minecraftVersion
+        )
+
+        inputs.properties(props)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
