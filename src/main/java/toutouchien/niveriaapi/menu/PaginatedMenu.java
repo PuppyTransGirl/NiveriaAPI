5c4252bac5c42a6a5e0cb0a8a093a9de8c1c892b
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-18 11:52:29 +0100

    Improve the menu api (#30)
    * Add some utilities methods
    
    * Improve menus slightly again
    
    * Micro-optimizations
    
    * Improvements again

--- src/main/java/toutouchien/niveriaapi/menu/PaginatedMenu.java
+++ src/main/java/toutouchien/niveriaapi/menu/PaginatedMenu.java
@@ -2,34 +2,53 @@ package toutouchien.niveriaapi.menu;
 
 import org.jetbrains.annotations.NotNull;
 import toutouchien.niveriaapi.menu.infos.MenuInfos;
+import toutouchien.niveriaapi.menu.items.MenuItem;
 
 public abstract class PaginatedMenu extends Menu {
 	protected int page;
 
-	public PaginatedMenu(@NotNull MenuInfos menuInfos) {
+    protected PaginatedMenu(@NotNull MenuInfos menuInfos) {
 		super(menuInfos);
 
-		page = 0;
+        this.page = 0;
 	}
 
 	protected void incrementPage(int i) {
+        if (i < 0)
+            throw new IllegalArgumentException("Increment value must be non-negative");
+
 		this.page += i;
 
-		inventory.clear();
-		(this.itemsCache = items()).forEach(menuItem -> inventory.setItem(menuItem.slot(), menuItem.itemStack()));
+		this.inventory.clear();
+
+        for (MenuItem menuItem : this.itemsCache = this.items())
+            this.inventory.setItem(menuItem.slot(), menuItem.itemStack());
 	}
 
 	protected void decreasePage(int i) {
 		if (this.page <= 0)
-			return;
+			throw new IllegalArgumentException("Cannot decrease page below 0");
 
 		this.page = Math.max(0, this.page - i);
 
-		inventory.clear();
-		(this.itemsCache = items()).forEach(menuItem -> inventory.setItem(menuItem.slot(), menuItem.itemStack()));
+        this.inventory.clear();
+
+        for (MenuItem menuItem : this.itemsCache = this.items())
+            this.inventory.setItem(menuItem.slot(), menuItem.itemStack());
 	}
 
 	public int page() {
 		return page;
 	}
+
+    public boolean firstPage() {
+        return this.page == 0;
+    }
+
+    public boolean lastPage(int maxPage) {
+        if (maxPage < 0)
+            throw new IllegalArgumentException("maxPage must be non-negative");
+
+        return this.page == maxPage;
+    }
 }
