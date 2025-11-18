a4c6894e76cabc6889045bff1c9de22791258e15
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-18 15:29:02 +0100

    Fix command initialization

--- src/main/java/toutouchien/niveriaapi/utils/CommandUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/CommandUtils.java
@@ -1,6 +1,7 @@
 package toutouchien.niveriaapi.utils;
 
 import io.papermc.paper.command.brigadier.CommandSourceStack;
+import org.bukkit.permissions.Permissible;
 
 public class CommandUtils {
     private CommandUtils() {
@@ -9,6 +10,6 @@ public class CommandUtils {
 
     public static boolean defaultRequirements(CommandSourceStack css, String permission) {
         return css.getSender().hasPermission(permission)
-                && css.getExecutor().hasPermission(permission);
+                && css instanceof Permissible perm && perm.hasPermission(permission);
     }
 }
