709d0e9ab242765df99e2245ba92bc824090e7d7
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-22 19:20:28 +0100

    Rename MiniMessage serializers and plaintext thing that is not useful

--- src/main/java/toutouchien/niveriaapi/utils/ComponentUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/ComponentUtils.java
@@ -3,26 +3,21 @@ package toutouchien.niveriaapi.utils;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.json.JSONComponentSerializer;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
-import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 
 public class ComponentUtils {
     private static final MiniMessage miniMessage = MiniMessage.miniMessage();
     private static final JSONComponentSerializer jsonSerializer = JSONComponentSerializer.json();
-    private static final LegacyComponentSerializer legacyAmpersandTextSerializer = LegacyComponentSerializer.legacyAmpersand();
-    private static final LegacyComponentSerializer legacySectionTextSerializer = LegacyComponentSerializer.legacySection();
-    private static final PlainTextComponentSerializer plainTextSerializer = PlainTextComponentSerializer.plainText();
 
     private ComponentUtils() {
         throw new IllegalStateException("Utility class");
     }
 
     // MiniMessage
-    public static String serializeMiniMessage(Component component) {
+    public static String serializeMM(Component component) {
         return miniMessage.serialize(component);
     }
 
-    public static Component deserializeMiniMessage(String input) {
+    public static Component deserializeMM(String input) {
         return miniMessage.deserialize(input);
     }
 
@@ -33,27 +28,5 @@ public class ComponentUtils {
 
     public static Component deserializeJson(String input) {
         return jsonSerializer.deserialize(input);
-    }
-
-    // Legacy Text
-    public static String serializeLegacyAmpersandText(Component component) {
-        return legacyAmpersandTextSerializer.serialize(component);
-    }
-
-    public static Component deserializeLegacyAmpersandText(String input) {
-        return legacyAmpersandTextSerializer.deserialize(input);
-    }
-
-    public static String serializeLegacySectionText(Component component) {
-        return legacySectionTextSerializer.serialize(component);
-    }
-
-    public static Component deserializeLegacySectionText(String input) {
-        return legacySectionTextSerializer.deserialize(input);
-    }
-
-    // Plain Text
-    public static String serializePlainText(Component component) {
-        return plainTextSerializer.serialize(component);
     }
 }
