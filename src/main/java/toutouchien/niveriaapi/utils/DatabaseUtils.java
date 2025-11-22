06c22af7ece4104ce1a491b00191f948684e11c6
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-22 18:38:44 +0100

    Separate DatabaseUtils and serialization methods

--- src/main/java/toutouchien/niveriaapi/utils/DatabaseUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/DatabaseUtils.java
@@ -1,39 +1,14 @@
 package toutouchien.niveriaapi.utils;
 
-import com.google.common.base.Preconditions;
 import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 public class DatabaseUtils {
     private DatabaseUtils() {
         throw new IllegalStateException("Utility class");
-    }
-
-    public static byte @NotNull [] serializeItemStack(@NotNull ItemStack itemStack) {
-        Preconditions.checkNotNull(itemStack, "itemStack cannot be null");
-        return itemStack.serializeAsBytes();
-    }
-
-    @Nullable
-    public static ItemStack deserializeItemStack(byte @NotNull [] serializedItemStack) {
-        Preconditions.checkNotNull(serializedItemStack, "serializedItemStack cannot be null");
-        return ItemStack.deserializeBytes(serializedItemStack);
-    }
-
-    public static byte @NotNull [] serializeItemStackArray(@NotNull ItemStack[] itemStacks) {
-        Preconditions.checkNotNull(itemStacks, "itemStacks cannot be null");
-        return ItemStack.serializeItemsAsBytes(itemStacks);
-    }
-
-    @NotNull
-    public static ItemStack[] deserializeItemStackArray(byte @NotNull [] serializedItemStacks) {
-        Preconditions.checkNotNull(serializedItemStacks, "serializedItemStacks cannot be null");
-        return ItemStack.deserializeItemsFromBytes(serializedItemStacks);
     }
 
     @NotNull
