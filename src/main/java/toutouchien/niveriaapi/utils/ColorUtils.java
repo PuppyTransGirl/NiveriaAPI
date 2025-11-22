e98868a5fa3c6dad764457869bffd766f2f0eccd
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-11-22 01:03:27 +0100

    Remove unused things

--- src/main/java/toutouchien/niveriaapi/utils/ColorUtils.java
+++ src/main/java/toutouchien/niveriaapi/utils/ColorUtils.java
@@ -18,20 +18,4 @@ public class ColorUtils {
     public static TextColor defaultMotdColor() {
         return TextColor.color(8421504);
     }
-
-    public static TextColor errorColor() {
-        return TextColor.fromHexString("#F52F2E");
-    }
-
-    public static TextColor infoColor() {
-        return TextColor.fromHexString("#364CD2");
-    }
-
-    public static TextColor successColor() {
-        return TextColor.fromHexString("#36C835");
-    }
-
-    public static TextColor warnColor() {
-        return TextColor.fromHexString("#FF6501");
-    }
 }
