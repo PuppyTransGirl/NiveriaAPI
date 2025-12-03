d3f5eacc22ffa6cea2d501924658d5ff3811fa51
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-12-04 00:14:31 +0100

    Move Icon from display to interactive

--- src/main/java/toutouchien/niveriaapi/menu/component/display/Icon.java
+++ src/main/java/toutouchien/niveriaapi/menu/component/display/Icon.java
@@ -1,4 +1,4 @@
-package toutouchien.niveriaapi.menu.component.interactive;
+package toutouchien.niveriaapi.menu.component.display;
 
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
