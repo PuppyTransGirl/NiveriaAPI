5c87dd7b15d9d93e702855b45ad4ce6c7a682267
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-12-06 21:35:52 +0100

    Add the first premade menu

---
+++ src/main/java/toutouchien/niveriaapi/menu/component/premade/ConfirmationMenu.java
@@ -0,0 +1,91 @@
+package toutouchien.niveriaapi.menu.component.premade;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import toutouchien.niveriaapi.menu.Menu;
+import toutouchien.niveriaapi.menu.component.display.Icon;
+import toutouchien.niveriaapi.menu.component.interactive.Button;
+import toutouchien.niveriaapi.menu.component.layout.Grid;
+import toutouchien.niveriaapi.menu.event.NiveriaInventoryClickEvent;
+import toutouchien.niveriaapi.utils.ItemBuilder;
+
+import java.util.function.Consumer;
+
+public class ConfirmationMenu extends Menu {
+    private final Component title, yesTitle, noTitle;
+    private final ItemStack explanationItem;
+    private final Consumer<NiveriaInventoryClickEvent> yesConsumer, noConsumer;
+
+    public ConfirmationMenu(
+            @NotNull Player player,
+            @NotNull Component title, @NotNull Component yesTitle, @NotNull Component noTitle,
+            @Nullable ItemStack explanationItem,
+            @NotNull Consumer<NiveriaInventoryClickEvent> yesConsumer, @NotNull Consumer<NiveriaInventoryClickEvent> noConsumer
+    ) {
+        super(player);
+
+        this.title = title;
+        this.yesTitle = yesTitle;
+        this.noTitle = noTitle;
+
+        this.explanationItem = explanationItem;
+
+        this.yesConsumer = yesConsumer;
+        this.noConsumer = noConsumer;
+    }
+
+    @NotNull
+    @Override
+    protected Component title() {
+        return this.title;
+    }
+
+    @NotNull
+    @Override
+    protected toutouchien.niveriaapi.menu.component.Component root() {
+        Grid.Builder builder = Grid.create()
+                .size(9, 3)
+                .add(11, yesButton())
+                .add(15, noButton());
+
+        if (this.explanationItem != null)
+            builder.add(13, explanationIcon());
+
+        return builder.build();
+    }
+
+    @NotNull
+    private Button yesButton() {
+        ItemStack yesItem = ItemBuilder.of(Material.LIME_DYE)
+                .name(this.yesTitle)
+                .build();
+
+        return Button.create()
+                .item(yesItem)
+                .onClick(this.yesConsumer)
+                .build();
+    }
+
+    @NotNull
+    private Button noButton() {
+        ItemStack noItem = ItemBuilder.of(Material.RED_DYE)
+                .name(this.noTitle)
+                .build();
+
+        return Button.create()
+                .item(noItem)
+                .onClick(this.noConsumer)
+                .build();
+    }
+
+    @NotNull
+    private Icon explanationIcon() {
+        return Icon.create()
+                .item(this.explanationItem)
+                .build();
+    }
+}
