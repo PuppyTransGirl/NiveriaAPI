36d16bf9c0cd6f3edc193a168c91baf448aa6e9b
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-12-07 12:24:09 +0100

    Clean commands

--- src/main/java/toutouchien/niveriaapi/commands/NiveriaAPITestCommand.java
+++ src/main/java/toutouchien/niveriaapi/commands/NiveriaAPITestCommand.java
@@ -22,30 +22,37 @@ public class NiveriaAPITestCommand {
         return Commands.literal("test")
                 .requires(css -> CommandUtils.defaultRequirements(css, "niveriaapi.command.niveriaapi.test"))
                 .requires(css -> css.getExecutor() instanceof Player)
-                .then(Commands.literal("basic")
-                        .executes(ctx -> {
-                            Player player = (Player) ctx.getSource().getExecutor();
-                            TestMenu menu = new TestMenu(player);
-                            menu.open();
+                .then(basicCommand())
+                .then(confirmationCommand());
+    }
 
-                            return Command.SINGLE_SUCCESS;
-                        }))
-                .then(Commands.literal("confirmation")
-                        .executes(ctx -> {
-                            Player player = (Player) ctx.getSource().getExecutor();
-                            ConfirmationMenu menu = new ConfirmationMenu(
-                                    player,
-                                    Component.text("Are you sure ?"),
-                                    Component.text("Yes", NamedTextColor.GREEN),
-                                    Component.text("No", NamedTextColor.RED),
-                                    ItemBuilder.of(Material.OAK_SIGN).name(Component.text("This action is irreversible")).build(),
-                                    event -> event.player().sendRichMessage("You clicked <green>Yes"),
-                                    event -> event.player().sendRichMessage("You clicked <red>No")
-                            );
-                            menu.open();
+    private static LiteralArgumentBuilder<CommandSourceStack> basicCommand() {
+        return Commands.literal("basic")
+                .executes(ctx -> {
+                    Player player = (Player) ctx.getSource().getExecutor();
+                    TestMenu menu = new TestMenu(player);
+                    menu.open();
 
-                            return Command.SINGLE_SUCCESS;
-                        })
-                );
+                    return Command.SINGLE_SUCCESS;
+                });
+    }
+
+    private static LiteralArgumentBuilder<CommandSourceStack> confirmationCommand() {
+        return Commands.literal("confirmation")
+                .executes(ctx -> {
+                    Player player = (Player) ctx.getSource().getExecutor();
+                    ConfirmationMenu menu = new ConfirmationMenu(
+                            player,
+                            Component.text("Are you sure ?"),
+                            Component.text("Yes", NamedTextColor.GREEN),
+                            Component.text("No", NamedTextColor.RED),
+                            ItemBuilder.of(Material.OAK_SIGN).name(Component.text("This action is irreversible")).build(),
+                            event -> event.player().sendRichMessage("You clicked <green>Yes"),
+                            event -> event.player().sendRichMessage("You clicked <red>No")
+                    );
+                    menu.open();
+
+                    return Command.SINGLE_SUCCESS;
+                });
     }
 }
