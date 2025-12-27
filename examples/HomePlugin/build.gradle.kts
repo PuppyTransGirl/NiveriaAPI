97861da25d983749f7c4af357d81fe361a2cec0e
PuppyTransGirl <74014559+PuppyTransGirl@users.noreply.github.com>
2025-12-27 17:10:13 +0100

    Start HomePlugin

---
+++ examples/HomePlugin/build.gradle.kts
@@ -0,0 +1,90 @@
+plugins {
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.19"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+val minecraftVersion: String by project
+val niveriaApiVersion: String by project
+
+group = "toutouchien.homeplugin"
+version = "1.0.0"
+
+repositories {
+    mavenLocal()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    paperweight.paperDevBundle("${minecraftVersion}-R0.1-SNAPSHOT")
+
+    // Plugins
+    compileOnly("toutouchien.niveriaapi:niveriaapi:${placeholderApiVersion}")
+}
+
+paperweight {
+    addServerDependencyTo = configurations.named(JavaPlugin.COMPILE_ONLY_CONFIGURATION_NAME).map { setOf(it) }
+}
+
+tasks {
+    runServer {
+        minecraftVersion(minecraftVersion)
+
+        jvmArgs(
+            "-Xmx4096M",
+            "-Xms4096M",
+            "-XX:+AllowEnhancedClassRedefinition",
+            "-XX:HotswapAgent=core",
+            "-Dcom.mojang.eula.agree=true"
+        )
+
+        downloadPlugins {
+            modrinth("LuckPerms", "v5.5.17-bukkit")
+            github("jpenilla", "TabTPS", "v1.3.29", "tabtps-paper-1.3.29.jar")
+            modrinth("ServerLogViewer-Paper", "1.0.0")
+        }
+    }
+
+    build {
+        dependsOn("jar")
+    }
+
+    test {
+        useJUnitPlatform()
+    }
+
+    javadoc {
+        isFailOnError = false
+        options.encoding = "UTF-8"
+    }
+
+    register<Jar>("javadocJar") {
+        dependsOn(javadoc)
+        archiveClassifier.set("javadoc")
+        from(javadoc.get().destinationDir)
+    }
+
+    register<Jar>("sourcesJar") {
+        archiveClassifier.set("sources")
+        from(sourceSets.main.get().allSource)
+    }
+
+    processResources {
+        filteringCharset = "UTF-8"
+
+        val props = mapOf(
+            "version" to version,
+            "minecraftVersion" to minecraftVersion
+        )
+
+        inputs.properties(props)
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
+}
+
+artifacts {
+    add("archives", tasks.named("sourcesJar"))
+    add("archives", tasks.named("javadocJar"))
+}
